043ca44eb0301b506910e271a18e47f5c7bde560
John Andrew Lim <johnandrewvlim@gmail.com>
2024-02-14 20:45:12 +0800

    1.1
    Practice Coding [JAVA]

---
+++ PracticeCodes/Java/abPractice1/Practice1Input.java
@@ -0,0 +1,16 @@
+package abPractice1;
+
+public class Practice1Input {
+
+    public static void main(String[] args) {
+
+        Practice1Main newDog = new Practice1Main();
+        newDog.setName("Aldous");
+        newDog.setColor("Brown");
+        newDog.setBreed("Poodle");
+        System.out.println("Dog Name: " + newDog.getDogName());
+        System.out.println("Color: " + newDog.getDogColor());
+        System.out.println("Breed: " + newDog.getDogBreed());
+    }
+    
+}
---
+++ PracticeCodes/Java/abPractice1/Practice1Main.java
@@ -0,0 +1,41 @@
+package abPractice1;
+
+public class Practice1Main {
+
+    private String name;
+    private String color;
+    private String breed;
+
+    public String getDogName()
+    {
+        return name;
+    }
+
+    public String getDogBreed()
+    {
+        return breed;
+    }
+
+    public String getDogColor()
+    {
+        return color;
+    }
+
+    public void setName(String DogName)
+    {
+        name = DogName;
+    }
+
+    public void setColor(String DogColor)
+    {
+        color = DogColor;
+    }
+
+    public void setBreed(String DogBreed)
+    {
+        breed = DogBreed;
+    }
+
+    
+
+}
